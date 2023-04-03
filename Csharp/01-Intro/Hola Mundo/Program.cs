cef2db37799c50a01379f918920e22dc5cde6596
Giancarlo <gvasquez2511@gmail.com>
2023-04-03 10:35:42 -0500

    ordenando el git

---
+++ Csharp/01-Intro/Hola Mundo/Program.cs
@@ -0,0 +1,4 @@
+﻿// see https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hola, Mundo");
---
+++ javascript/mi-primer-projecto/index.js
@@ -0,0 +1,8 @@
+
+// Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al projecto")
+
+
+// Este va a sedr el mensaje que se envia para despedir
+
+console.log("Adios")
